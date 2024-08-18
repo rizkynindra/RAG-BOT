9690ab85efb43d6baf48e21c339f9ea9daa6d91a
rizky nindra <rizkynindra.sukma@bpjsketenagakerjaan.go.id>
2024-08-18 15:26:30 +0700

    jakarta hari ini

--- run_rag.py
+++ run_rag.py
@@ -4,31 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 key = os.getenv("OPENAI_API_KEY")
-assistant_id = os.getenv("ASSISTANT_ID")
+assist_id = os.getenv("ASSISTANT_ID")
+vect_id = os.getenv("VECTOR_ID")
 
 client = OpenAI(api_key=key)
 
-#create vector store
-vector_store = client.beta.vector_stores.create(name="qnatk")
-print(f"Vector Store Id - {vector_store.id}")
-
-#upload files
-file_paths = ["doc/FAQ_2023.pdf"]
-file_streams = [open(path, "rb") for path in file_paths]
-
-#add files to vector store
-file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
-    vector_store_id= vector_store.id,
-    files = file_streams
-)
-
-#check the status of files
-print(f"File status {file_batch.status}")
-
 #Update the assistant with a vector store
 assistant = client.beta.assistants.update(
-    assistant_id=assistant_id,
-    tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
+    assistant_id=assist_id,
+    tool_resources={"file_search": {"vector_store_ids": [vect_id]}},
 )
 
 print("Assistant updated with vector store!")
@@ -39,7 +23,7 @@ print(f"Your thread id is - {thread.id}\n\n")
 
 # Run a loop where user can ask questions
 while True:
-    text = input("Halo ada yang bisa dibantu?\n")
+    text = input("Mau nanya apa bro?\n")
 
     message = client.beta.threads.messages.create(
         thread_id=thread.id,
